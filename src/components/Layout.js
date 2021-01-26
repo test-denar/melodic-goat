7840168defbedb8e4d234f045f90683e93c5e693
Stackbit <projects@stackbit.com>
2021-01-26 16:02:36 +0200

    initial commit by Stackbit

---
+++ src/components/Layout.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import _ from 'lodash';
+
+import {withPrefix} from '../utils';
+import Header from './Header';
+import Footer from './Footer';
+
+export default class Body extends React.Component {
+    render() {
+        let font = _.get(this.props, 'data.config.base_font', null) || 'nunito-sans';
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>{_.get(this.props, 'page.seo.title', null) ? (_.get(this.props, 'page.seo.title', null)) : _.get(this.props, 'page.title', null) + ' | ' + _.get(this.props, 'data.config.title', null)}</title>
+                    <meta charSet="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="google" content="notranslate" />
+                    <meta name="description" content={_.get(this.props, 'page.seo.description', null) || ''} />
+                    {_.get(this.props, 'page.seo.robots', null) && (
+                    <meta name="robots" content={_.join(_.get(this.props, 'page.seo.robots', null), ',')}/>
+                    )}
+                    {_.get(this.props, 'page.seo.canonical_url', null) ? (
+                    <link rel="canonical" href={_.get(this.props, 'page.seo.canonical_url', null)} />
+                    ) : (_.get(this.props, 'data.config.domain', null) && ((() => {
+                        let domain = _.trim(_.get(this.props, 'data.config.domain', null), '/');
+                        let page_rel_url = withPrefix(_.get(this.props, 'page.stackbit_url_path', null));
+                        return (
+                          <link rel="canonical" href={domain + page_rel_url} />
+                        );
+                    })()))}
+                    {_.map(_.get(this.props, 'page.seo.extra', null), (meta, meta_idx) => (
+                      ((_.get(meta, 'name', null) === 'og:image') || (_.get(meta, 'name', null) === 'twitter:image')) ? (
+                        _.get(meta, 'value', null).startsWith('http') ? (
+                        <meta key={meta_idx} {...(_.get(meta, 'name', null).startsWith('og:') ? ({property: _.get(meta, 'name', null)}) : {name: _.get(meta, 'name', null)})} content={_.get(meta, 'value', null)}/>
+                        ) : (_.get(this.props, 'data.config.domain', null) && ((() => {
+                            let domain = _.trim(_.get(this.props, 'data.config.domain', null), '/');
+                            let image_rel_url = withPrefix(_.get(meta, 'value', null));
+                            return (
+                              <meta key={meta_idx + '.1'} {...(_.get(meta, 'name', null).startsWith('og:') ? ({property: _.get(meta, 'name', null)}) : {name: _.get(meta, 'name', null)})} content={domain + image_rel_url}/>
+                            );
+                        })()))
+                      ) : 
+                        <meta key={meta_idx + '.2'} {...(_.get(meta, 'name', null).startsWith('og:') ? ({property: _.get(meta, 'name', null)}) : {name: _.get(meta, 'name', null)})} content={_.get(meta, 'value', null)}/>
+                    ))}
+                    {(font !== 'system-sans') && (
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    )}
+                    {(font === 'nunito-sans') ? (
+                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                    ) : ((font === 'fira-sans') && (
+                    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet"/>
+                    ))}
+                    {_.get(this.props, 'data.config.favicon', null) && (
+                    <link rel="icon" href={withPrefix(_.get(this.props, 'data.config.favicon', null))}/>
+                    )}
+                    <body className={'palette-' + _.get(this.props, 'data.config.palette', null) + ' font-' + _.get(this.props, 'data.config.base_font', null)} />
+                </Helmet>
+                <div id="page" className="site">
+                  <Header {...this.props} />
+                  <main id="content" className="site-content">
+                    {this.props.children}
+                  </main>
+                  <Footer {...this.props} />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
